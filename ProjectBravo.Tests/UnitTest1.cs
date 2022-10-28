a4b8c67f3f3ff9e6c7739ea1c0bad470f7596ab3
emjakobsen1 <erja@itu.dk>
2022-10-28 14:46:34 +0200

    small refactor and testing

--- ProjectBravo.Tests/UnitTest1.cs
+++ ProjectBravo.Tests/UnitTest1.cs
@@ -1,10 +1,58 @@
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
 namespace ProjectBravo.Tests;
 
-public class UnitTest1
+public sealed class ProgramTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly string _gitRepoPath = @"../../../git-test-repos/git";
 
+    [Fact]
+    public void Program_given_commit_frequency()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        // Act
+        var program = Assembly.Load(nameof(ProjectBravo));
+
+        program.EntryPoint?.Invoke(null, new[] { new string[] { _gitRepoPath, "frequency" } });
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        var expected = 
+            @"8 2022-09-21
+2 2022-09-18
+8 2022-09-17
+3 2022-09-16
+2 2022-09-15
+1 2022-09-14
+5 2021-09-17
+2 2021-09-16";
+
+        Assert.Equal(expected, output);
+    }
+    [Fact]
+    public void Program_given_commit_author_frequency()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        // Act
+        var program = Assembly.Load(nameof(ProjectBravo));
+
+        program.EntryPoint?.Invoke(null, new[] { new string[] { _gitRepoPath, "author" } });
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        var expected = 
+            "emjakobsen1\r\n\t6 2022-09-21\r\n\t1 2022-09-18\r\n\t7 2022-09-17\r\n\r\nrakulmaria\r\n\t1 2022-09-21\r\n\r\nGustav\r\n\t1 2022-09-21\r\n\r\nrakul\r\n\t1 2022-09-18\r\n\t1 2022-09-17\r\n\r\nHelgeCPH\r\n\t2 2022-09-16\r\n\t1 2022-09-15\r\n\r\nRasmus Lystrøm\r\n\t1 2022-09-16\r\n\t1 2022-09-15\r\n\t1 2022-09-14\r\n\t2 2021-09-16\r\n\r\nPaolo Tell\r\n\t5 2021-09-17";
+
+        Assert.Equal(expected, output);
     }
 }
--- ProjectBravo/Program.cs
+++ ProjectBravo/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibGit2Sharp;
+using System.Globalization;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args[1] == "frequency")
+        {
+            Frequency(args[0]);
+        }
+        if (args[1] == "author")
+        {
+            Author(args[0]);
+        }
+    }
+
+    private static void Frequency(string repository)
+    {
+        using var repo = new Repository(repository);
+            repo.Commits
+                .GroupBy(commit => commit.Author.When.Date)
+                .ToList()
+                .ForEach(
+                    x =>
+                        Console.WriteLine(
+                            $"{x.Count()} {x.Key.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)}"
+                        )
+                );
+    }
+
+    private static void Author(string repository)
+    {
+        using var repo = new Repository(repository);
+                {
+                    var dateFormat = "yyyy-MM-dd";
+        
+                    var dateGroups = repo.Commits.GroupBy(
+                        commit => commit.Author.When.Date
+                    );
+                    Dictionary<string, List<Commit>> authorToCommits = new Dictionary<string, List<Commit>>();
+        
+                    var authors = repo.Commits.Select(commit => commit.Author.Name).Distinct();
+                    foreach (var author in authors)
+                    {
+                        authorToCommits.Add(author, new List<Commit>());
+                        foreach (var commit in repo.Commits)
+                        {
+                            if (commit.Author.Name == author)
+                            {
+                                authorToCommits[author].Add(commit);
+                            }
+                        }
+                    }
+        
+                    foreach (var author in authorToCommits.Keys)
+                    {
+                        Console.WriteLine(author);
+        
+                        var authorDateCommits = authorToCommits[author].GroupBy(
+                        commit => commit.Author.When.Date
+                    );
+                        foreach (var dateGroup in authorDateCommits)
+                        {
+                            Console.WriteLine($"\t{dateGroup.Count()} {dateGroup.Key.ToString(dateFormat, CultureInfo.InvariantCulture)}");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+    }
+}
