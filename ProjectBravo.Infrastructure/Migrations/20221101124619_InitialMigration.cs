2ac8bbbefce83172a7f304c4c79dde06ed1014dd
tbru <tbru@itu.dk>
2022-11-01 13:54:03 +0100

    add missing packages and migrations

---
+++ ProjectBravo.Infrastructure/Migrations/20221101124619_InitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectBravo.Infrastructure.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "repos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FrequencyOutput = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AuthorOutput = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LatestCommitDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_repos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "repos");
+        }
+    }
+}
