948c10e6d26347247bba79654d7e9ad9a2d62e43
emjakobsen1 <98593540+emjakobsen1@users.noreply.github.com>
2022-11-21 19:45:06 +0100

    Create Utilities.cs

---
+++ ProjectBravo/Utilities.cs
@@ -0,0 +1,41 @@
+using System.Net;
+using ProjectBravo.Core;
+
+public class Utilities
+{
+
+
+    private static HttpStatusCode StatusConversion(Status status) => status switch
+    {
+        Status.OK => HttpStatusCode.OK,
+        Status.Created => HttpStatusCode.Created,
+        Status.Deleted => HttpStatusCode.OK,
+        Status.Updated => HttpStatusCode.NoContent,
+        Status.NotFound => HttpStatusCode.NotFound,
+        Status.Conflict => HttpStatusCode.Conflict,
+        _ => HttpStatusCode.BadRequest
+    };
+
+
+
+
+    public static void setAttributesNormal(DirectoryInfo dir)
+    {
+        foreach (var subDir in dir.GetDirectories())
+            setAttributesNormal(subDir);
+        foreach (var file in dir.GetFiles())
+        {
+            file.Attributes = FileAttributes.Normal;
+        }
+    }
+
+
+    public static void setAttributesNormalLINQ(DirectoryInfo dir) =>
+        dir.GetDirectories("*", SearchOption.AllDirectories)
+        .Where(dir => !dir.GetDirectories().Any()).ToList()
+        .ForEach(subDir => subDir.GetFiles().ToList()
+        .ForEach(file => file.Attributes = FileAttributes.Normal));
+
+}
+
+
